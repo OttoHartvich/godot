6993db2c210aecbb1d30e080ddad1a7b887882fc
OttoHartvich <suckots@gmail.com>
2024-06-30 20:47:40 +0200

    fixed c++ bugs

--- modules/fluid_simulation/fluid_simulation.cpp
+++ modules/fluid_simulation/fluid_simulation.cpp
@@ -1,111 +1,133 @@
-/* fluid_simulation.cpp */
-
 #include "fluid_simulation.h"
+#include "core/object/ref_counted.h"
+#include <Slot.h>
+#include <array>
+#include <cmath>
+#include <pairpos-graph.hh>
+#include <vector>
 
-void FluidSimulator:: _populate_UV_neighbors() {
-    for (int i = 0; i < size_of(UV_neighbors); ++i) {
-        int x = i % Width;
-        int y = floor(i / Width);
-        int left = (2 * Width + 1) * y + x + Width + 1;
-        int right = (2 * Width + 1) * y + x + Width + 2;
-        int up = (2 * Width + 1) * y + x;
-        int down = (2 * Width + 1) * y + x + 2 * Width + 1;
-        
-        UV_neighbors[i] = {left, right, up, down};
-    }
+FluidSimulator::FluidSimulator(int width, int height, int step, float density, float gridSpacing) :
+		Width(width),
+		Height(height),
+		Physics_step(step),
+		Density(density),
+		GridSpacing(gridSpacing),
+		XY(width * height, 0.0f),
+		CollisionMap(width * height, 1),
+		PressureMap(width * height, 0.0f),
+		UV((2 * width + 1) * (height / 2) + ((height % 2 == 0) ? 0 : width + 1), 0.0f),
+		NewUV((2 * width + 1) * (height / 2) + ((height % 2 == 0) ? 0 : width + 1), 0.0f),
+		XY_neighbors(width * height),
+		UV_neighbors((2 * width + 1) * (height / 2) + ((height % 2 == 0) ? 0 : width + 1)),
+		UV_to_UV_average_neighbors((2 * width + 1) * (height / 2) + ((height % 2 == 0) ? 0 : width + 1)) {
+	_populate_XY_neighbors();
+	_populate_UV_neighbors();
+	_populate_UV_to_UV_average_neighbors(UV.size());
 }
 
-void FluidSimulator:: _populate_XY_neighbors() {
-    for (int i = 0; i < XY_neighbors.size(); ++i) {
-        int left = (i % Width == 0) ? -1 : i - 1;
-        int right = (i % Width == Width - 1) ? -1 : i + 1;
-        int up = (i < Width) ? -1 : i - Width;
-        int down = (i >= (Height - 1) * Width) ? -1 : i + Width;
-
-        XY_neighbors[i] = {left, right, up, down};
-    }
+void FluidSimulator::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("_populate_UV_neighbors"), &FluidSimulator::_populate_UV_neighbors);
+	ClassDB::bind_method(D_METHOD("_populate_XY_neighbors"), &FluidSimulator::_populate_XY_neighbors);
+	ClassDB::bind_method(D_METHOD("_populate_UV_to_UV_average_neighbors", "UV_size"), &FluidSimulator::_populate_UV_to_UV_average_neighbors);
+	ClassDB::bind_method(D_METHOD("_solve_incompressibility", "delta"), &FluidSimulator::_solve_incompressibility);
+	ClassDB::bind_method(D_METHOD("delta_step", "delta"), &FluidSimulator::delta_step);
 }
 
-void FluidSimulator:: _solve_incompressibility(float delta) {
+void FluidSimulator::_populate_UV_neighbors() {
+	for (int i = 0; i < UV_neighbors.size(); ++i) {
+		int x = i % Width;
+		int y = std::floor(i / Width);
+		int left = (2 * Width + 1) * y + x + Width + 1;
+		int right = (2 * Width + 1) * y + x + Width + 2;
+		int up = (2 * Width + 1) * y + x;
+		int down = (2 * Width + 1) * y + x + 2 * Width + 1;
+
+		UV_neighbors[i] = { left, right, up, down };
+	}
+}
+
+void FluidSimulator::_populate_XY_neighbors() {
+	for (int i = 0; i < XY_neighbors.size(); ++i) {
+		int left = (i % Width == 0) ? -1 : i - 1;
+		int right = (i % Width == Width - 1) ? -1 : i + 1;
+		int up = (i < Width) ? -1 : i - Width;
+		int down = (i >= (Height - 1) * Width) ? -1 : i + Width;
+
+		XY_neighbors[i] = { left, right, up, down };
+	}
+}
+
+void FluidSimulator::_populate_UV_to_UV_average_neighbors(int UV_size) {
+	for (int i = 0; i < UV_to_UV_average_neighbors.size(); ++i) {
+		if (i % (2 * Width + 1) <= (Width + 1)) {
+			int leftUp = (i - (Width + 1)) < 0 ? 0 : (i - (Width + 1));
+			int rightUp = i - Width;
+			int leftDown = (i < Width) ? -1 : i - Width;
+			int rightDown = (i + Width + 1) > UV_size ? 0 : (i + Width + 1);
+
+			UV_to_UV_average_neighbors[i] = { leftUp, rightUp, leftDown, rightDown };
+		} else {
+			int leftUp = (i - (Width + 1)) < 0 ? 0 : (i - (Width + 1));
+			int rightUp = i - Width;
+			int leftDown = (i < Width) ? -1 : i - Width;
+			int rightDown = (i + Width + 1) > UV_size ? 0 : (i + Width + 1);
+
+			UV_to_UV_average_neighbors[i] = { leftUp, rightUp, leftDown, rightDown };
+		}
+	}
+}
+
+void FluidSimulator::_solve_incompressibility(float delta) {
     for (int i = 0; i <= Physics_step; ++i) {
         for (int xy = 0; xy < XY.size(); ++xy) {
-            // if there is no wall it will break
             if (CollisionMap[xy] == 0) {
                 continue;
             }
-            int collisionLeft = CollisionMap[XY_neighbors[xy].[0]] == -1 ? 0 : CollisionMap[XY_neighbors[xy].[0]];
-            int collisionRight = CollisionMap[XY_neighbors[xy].[1]] == -1 ? 0 : CollisionMap[XY_neighbors[xy].[1]];
-            int collisionUp = CollisionMap[XY_neighbors[xy].[2]] == -1 ? 0 : CollisionMap[XY_neighbors[xy].[2]];
-            int collisionDown = CollisionMap[XY_neighbors[xy].[3]] == -1 ? 0 : CollisionMap[XY_neighbors[xy].[3]];
-
+            int collisionLeft = (XY_neighbors[xy][0] == -1) ? 0 : CollisionMap[XY_neighbors[xy][0]];
+            int collisionRight = (XY_neighbors[xy][1] == -1) ? 0 : CollisionMap[XY_neighbors[xy][1]];
+            int collisionUp = (XY_neighbors[xy][2] == -1) ? 0 : CollisionMap[XY_neighbors[xy][2]];
+            int collisionDown = (XY_neighbors[xy][3] == -1) ? 0 : CollisionMap[XY_neighbors[xy][3]];
 
             if (collisionLeft == 0 && collisionRight == 0 && collisionUp == 0 && collisionDown == 0) {
                 continue;
             }
             int collisionSum = collisionLeft + collisionRight + collisionUp + collisionDown;
-            float div = - UV[UV_neighbors[xy].[0]] + UV[UV_neighbors[xy].[1]] + UV[UV_neighbors[xy].[2]] - UV[UV_neighbors[xy].[3]];
-            float pressure = (- div / collisionSum) * 1.9;
+            float div = -UV[UV_neighbors[xy][0]] + UV[UV_neighbors[xy][1]] + UV[UV_neighbors[xy][2]] - UV[UV_neighbors[xy][3]];
+            float pressure = (-div / collisionSum) * 1.9f; // Ensure 1.9 is treated as float
+
             if (i == Physics_step) {
                 float pressureQuotient = Density * GridSpacing / delta; 
                 PressureMap[xy] = pressure * pressureQuotient;
             }
-            UV[UV_neighbors[xy].[0]] += collisionLeft * pressure;
-            UV[UV_neighbors[xy].[1]] -= collisionRight * pressure;
-            UV[UV_neighbors[xy].[2]] -= collisionUp * pressure;
-            UV[UV_neighbors[xy].[3]] += collisionDown * pressure;
+            UV[UV_neighbors[xy][0]] += collisionLeft * pressure;
+            UV[UV_neighbors[xy][1]] -= collisionRight * pressure;
+            UV[UV_neighbors[xy][2]] -= collisionUp * pressure;
+            UV[UV_neighbors[xy][3]] += collisionDown * pressure;
         }
     }
 }
 
 void FluidSimulator::_make_box_around_collisionMap() {
-    for (int i = 0; i < CollisionMap.size(); ++i) {
-        if (i < Width) {
-            CollisionMap[i] = 0;
-            continue;
-        }
-        if ((i + 1) % Width == (Width -1)) {
-            CollisionMap[i] = 0;
-            continue;
-        }
-        if ((i + 1 ) % Width == 1) {
-            CollisionMap[i] = 0;
-            continue;
-        }
-        if (i >= (Width * Height - Width)) {
-            CollisionMap[i] = 0;
-            continue;
-        }
-    }
+	for (int i = 0; i < CollisionMap.size(); ++i) {
+		if (i < Width) {
+			CollisionMap[i] = 0;
+			continue;
+		}
+		if ((i + 1) % Width == (Width - 1)) {
+			CollisionMap[i] = 0;
+			continue;
+		}
+		if ((i + 1) % Width == 1) {
+			CollisionMap[i] = 0;
+			continue;
+		}
+		if (i >= (Width * Height - Width)) {
+			CollisionMap[i] = 0;
+			continue;
+		}
+	}
 }
 
 void FluidSimulator::delta_step(float delta) {
-    _solve_incompressibility(delta);
-}
-
-void FluidSimulator::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("init", "width", "height"), &FluidSimulator::init);
-    ClassDB::bind_method(D_METHOD("delta_step", "delta"), &FluidSimulator::init);
-}
-
-FluidSimulator::FluidSimulator(int width, int height, int step, float density, float gridSpacing) {
-	Density(density);
-    GridSpacing(gridSpacing);
-    Width(width);
-    Height(height);
-    Physics_step(step);
-    int XY_size = Width * Height;
-    int UV_size = (2 * Width + 1) * Height / 2; 
-    if (!Height % 2 ==0) {
-        UV_size += Width + 1;
-    };
-    XY(XY_size, 0.0f);
-    CollisionMap(XY_size,1);
-    PressureMap(XY_size,0.0f);
-    UV(UV_size, 0.0f);
-    // pre-calculates the neighbor indexes of XY to XY
-    XY_neighbors(XY_size, int arr[4]);
-    // pre-calculates the neighbor indexes of UV to XY
-    UV_neighbors(XY_size, int arr[4]);
-    populate_XY_neighbors();
-    populate_UV_neighbors();
+	_solve_incompressibility(delta);
 }
--- modules/fluid_simulation/fluid_simulation.h
+++ modules/fluid_simulation/fluid_simulation.h
@@ -1,38 +1,39 @@
-/* fluid_simulation.h */
-
-#include <iostream>
-#include <vector>
 #ifndef FLUID_SIMULATOR_H
 #define FLUID_SIMULATOR_H
 
 #include "core/object/ref_counted.h"
+#include <iostream>
+#include <vector>
+#include <array>
 
 class FluidSimulator : public RefCounted {
-	GDCLASS(FluidSimulator, RefCounted);
+    GDCLASS(FluidSimulator, RefCounted);
+
     int Width;
     int Height;
     int Physics_step;
     float Density;
     float GridSpacing;
-	std::vector<float> XY;
+    std::vector<float> XY;
     std::vector<std::array<int, 4>> XY_neighbors;
     std::vector<float> UV;
+    std::vector<float> NewUV;
     std::vector<std::array<int, 4>> UV_neighbors;
+    std::vector<std::array<int, 4>> UV_to_UV_average_neighbors;
     std::vector<int> CollisionMap;
     std::vector<float> PressureMap;
 
-    
 protected:
-	static void _bind_methods();
+    static void _bind_methods();
     void _populate_UV_neighbors();
     void _populate_XY_neighbors();
+    void _populate_UV_to_UV_average_neighbors(int UV_size);
     void _solve_incompressibility(float delta);
     void _make_box_around_collisionMap();
-public:
-    void delta_step(float delta);
-	//int get_total() const;
 
-	FluidSimulator(int width,int height, int step, float density, float gridSpacing);
+public:
+    FluidSimulator(int width, int height, int step, float density, float gridSpacing);
+    void delta_step(float delta);
 };
 
 #endif // FLUID_SIMULATOR_H
